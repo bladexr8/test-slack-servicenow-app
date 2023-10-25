7f8882343fbc618a9418bff3759f86f584aba64e
Stephen Moss <stephenmoss@brooke.global>
2023-10-25 14:23:28 +1100

    update outgoing domain

--- manifest.ts
+++ manifest.ts
@@ -13,6 +13,6 @@ export default Manifest({
   icon: "assets/default_new_app_icon.png",
   functions: [CreateIncidentDefinition],
   workflows: [CreateNewIncidentWorkflow],
-  outgoingDomains: ["servicenow.com"],
+  outgoingDomains: ["service-now.com"],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
